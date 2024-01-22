921a30702889f94a7e9080b96096d2c36f17e71a
Pankaj Gaikwad <gaikwadpankaj62@gmail.com>
2024-01-23 00:34:05 +0530

    update~

--- components/LoginForm.jsx
+++ components/LoginForm.jsx
@@ -64,7 +64,8 @@ export default function LoginForm() {
           )}
 
           <Link href={'/register'} className='text-sm mt-3 text-right'>
-            Don't have an account? <span className='underline'>Register</span>
+            Don&apos;t have an account?{' '}
+            <span className='underline'>Register</span>
           </Link>
         </form>
       </div>
